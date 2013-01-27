401e5ba63e60b3fb9b2e2def6e68b2c751f2d1f8
nrathi <neharrathi@gmail.com>
2013-01-27 12:30:04 -0500

    My first commit.

--- src/org/techbridgeworld/bwt/MainActivity.java
+++ src/org/techbridgeworld/bwt/MainActivity.java
@@ -2,21 +2,45 @@ package org.techbridgeworld.bwt;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.TextView;
+import android.hardware.usb.*;
 
 public class MainActivity extends Activity {
+	
+	String s = ""; 
+	
+	TextView textView; 
+	UsbDevice usbDevice;
+	UsbManager usbManager; 
+	UsbInterface usbInterface;
+	UsbDeviceConnection usbConnection;
+	UsbEndpoint usbEndpoint;
+	
+	private byte[] bytes;
+	private static int TIMEOUT = 0;
+	private boolean forceClaim = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+        textView = (TextView) findViewById(R.id.textview);
+        
+        Intent intent = getIntent(); 
+        usbDevice = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+		usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+		
+		usbInterface = usbDevice.getInterface(0);
+		
+		usbEndpoint = usbInterface.getEndpoint(0);
+        
+		usbConnection = usbManager.openDevice(usbDevice); 
+		
+		usbConnection.claimInterface(usbInterface, forceClaim);
+		
+		//usbConnection.bulkTransfer(usbEndpoint, bytes, bytes.length, TIMEOUT);
     }
-    
 }
