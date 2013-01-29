02031cf583ade821cde6a5052ea4099e9cfe790e
nrathi <neharrathi@gmail.com>
2013-01-28 22:11:54 -0500

    ALL OF THEM.

--- src/org/techbridgeworld/bwt/MainActivity.java
+++ src/org/techbridgeworld/bwt/MainActivity.java
@@ -1,25 +1,30 @@
 package org.techbridgeworld.bwt;
 
-import android.os.Bundle;
+import java.nio.charset.Charset;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
-import android.hardware.usb.*;
 
 public class MainActivity extends Activity {
-	
-	String s = ""; 
 	
 	TextView textView; 
 	UsbDevice usbDevice;
 	UsbManager usbManager; 
 	UsbInterface usbInterface;
 	UsbDeviceConnection usbConnection;
-	UsbEndpoint usbEndpoint;
+	UsbEndpoint usbEndpointOut;
+	UsbEndpoint usbEndpointIn;
 	
-	private byte[] bytes;
-	private static int TIMEOUT = 0;
+	private static int TIMEOUT = 10000;
 	private boolean forceClaim = true;
 
     @Override
@@ -35,12 +40,59 @@ public class MainActivity extends Activity {
 		
 		usbInterface = usbDevice.getInterface(0);
 		
-		usbEndpoint = usbInterface.getEndpoint(0);
-        
+		//Endpoint 0 is in, Endpoint 1 is out
+		usbEndpointIn = usbInterface.getEndpoint(0);
+		usbEndpointOut = usbInterface.getEndpoint(1);
+		
 		usbConnection = usbManager.openDevice(usbDevice); 
 		
 		usbConnection.claimInterface(usbInterface, forceClaim);
 		
-		//usbConnection.bulkTransfer(usbEndpoint, bytes, bytes.length, TIMEOUT);
+//		try {
+//			usbConnection.controlTransfer(0x21, 34, 0, 0, null, 0, 0);
+//			usbConnection.controlTransfer(0x21, 32, 0, 0, new byte[] { (byte) 0x80,
+//					0x25, 0x00, 0x00, 0x00, 0x00, 0x08 }, 7, 0);
+//		}
+//		catch(Exception e) {
+//			textView.setText(e.getMessage());
+//		}
+		
+		usbConnection.controlTransfer(0x20, 34, 0, 0, null, 0, 0);
+		usbConnection.controlTransfer(0x20, 32, 0, 0, new byte[] { (byte) 0x80,
+				0x25, 0x00, 0x00, 0x00, 0x00, 0x08 }, 7, 0);
+		usbConnection.controlTransfer(0x20, 0x03, 0x0034, 0, null, 0, 0);
+		
+		String b = "b";  
+	    byte[] bytes = b.getBytes();
+		Integer result = usbConnection.bulkTransfer(usbEndpointOut, bytes, bytes.length, TIMEOUT);
+		if (result > 0) { textView.setText(result +  " bytes of data transferred!!"); }
+		
+		String t = "t";  
+	    bytes = t.getBytes();
+		result = usbConnection.bulkTransfer(usbEndpointOut, bytes, bytes.length, TIMEOUT);
+        if (result > 0) { textView.setText(result +  " bytes of data transferred!! x2"); }
+		
+		final Handler handler = new Handler();
+		final Runnable r = new Runnable()
+		{
+		    public void run() 
+		    {
+	            byte[] buffer = new byte[64];
+
+	            StringBuilder s = new StringBuilder(); 
+
+	 			if(usbConnection.bulkTransfer(usbEndpointIn, buffer, 64, TIMEOUT) >= 0) {
+	 				for(int i = 0; i < 64; i++) {
+	 					//if(buffer[i] != 0)
+	 					s.append(buffer[i]);
+	 				}
+	 			}
+
+	 			textView.setText(textView.getText() + "\n" + s); 
+		        handler.postDelayed(this, 1000);
+		    }
+		};
+
+		handler.postDelayed(r, 1000);
     }
 }
