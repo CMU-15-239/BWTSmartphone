7533bb88e739edfc459962009e3e33e11c5caeda
Salem <salemhilal@gmail.com>
2013-02-10 14:37:56 -0500

    Event update. Also added ChangeCellEvent.

--- src/org/techbridgeworld/bwtApi/events/BoardEvent.java
+++ src/org/techbridgeworld/bwtApi/events/BoardEvent.java
@@ -4,5 +4,15 @@ import javaEventing.EventObject;
 
 
 public class BoardEvent extends EventObject{
+
+	String message;
 	
+	public BoardEvent(String message){
+		this.message = message;
+	}
+	
+	// Getters
+	public String getMessage(){
+		return message;
+	}
 }
