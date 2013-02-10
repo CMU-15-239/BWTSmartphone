b2e0700ec4f52d0a01f793f3c7c7242f781b687f
nrathi <neharrathi@gmail.com>
2013-02-10 15:30:14 -0500

    beginning of UI

--- src/org/techbridgeworld/bwt/MainActivity.java
+++ src/org/techbridgeworld/bwt/MainActivity.java
@@ -1,58 +1,115 @@
 	package org.techbridgeworld.bwt;
 	
-	import org.techbridgeworld.bwtApi.BWT;
+	import java.util.Locale;
+
+import org.techbridgeworld.bwtApi.BWT;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ScrollView;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.view.GestureDetectorCompat;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.TextView;
 	
-	public class MainActivity extends Activity {
-		
-		// UI stuff
-		private ScrollView scrollView; 
-		private TextView textView; 
-		
-		// BWT object
+	public class MainActivity extends Activity implements TextToSpeech.OnInitListener {
+
+	    private static final int SWIPE_MIN_DISTANCE = 120;
+	    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+	    private GestureDetectorCompat detector; 
+
 		private final BWT bwt = new BWT(this, MainActivity.this);
 		
+		private TextView teacher_welcome;
+		private TextToSpeech tts;
 		
 	    @Override
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.main);
-	        scrollView = (ScrollView) findViewById(R.id.scrollview);
-	        textView = (TextView) findViewById(R.id.textview);
-	        textView.append("NOTE onCreate, created stuff\n");
+	        setContentView(R.layout.teacher_welcome);
 	        
 	        bwt.init();
+	        tts = new TextToSpeech(this, this);
+	        teacher_welcome = (TextView) findViewById(R.id.teacher_welcome);
+	        detector = new GestureDetectorCompat(this, new MyGestureListener());
+	    }
+	    
+	    @Override 
+	    public boolean onTouchEvent(MotionEvent event){ 
+	        this.detector.onTouchEvent(event);
+	        return super.onTouchEvent(event);
 	    }
 
 	    @Override
 	    protected void onPause() {
 	        super.onPause();
-	        textView.append("NOTE onPause, about to stopIoManager()\n");
-	        
 	        bwt.stop();
 	    }
 	    
 	    @Override
 	    protected void onResume() {	    	
-	        super.onResume();
-	        textView.append("NOTE onResume, about to deviceStateChange()\n");
-	        
+	        super.onResume();	        
 	        bwt.start();
 	    }
 	    
-	    // Used to print simple text to the screen
-	    private void display(String text){
-	        textView.append(text);
-	    	scrollView.smoothScrollTo(0, textView.getBottom());
+	    @Override
+	    public void onInit(int status) {
+	 
+	        if (status == TextToSpeech.SUCCESS) {
+	 
+	            int result = tts.setLanguage(Locale.US);
+	 
+	            if (result == TextToSpeech.LANG_MISSING_DATA
+	                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+	                Log.e("TTS", "This Language is not supported");
+	            } else {
+	                speakOut();
+	            }
+	 
+	        } 
+	        else {
+	            Log.e("TTS", "Initilization Failed!");
+	        }
 	    }
 	    
-	    // Prints simple text + a line break
-	    private void displayln(String text){
-	        display(text + "\n");
+	    private void speakOut() {
+	        String text = teacher_welcome.getText().toString();
+	        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
 	    }
+	    
+	    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+	    	
+	        @Override
+	        public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
 
+	        	// Swipe up
+                if (event1.getY() - event2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+            		//Intent intent = new Intent(MainActivity.this, LanguageActivity.class);
+            		//startActivity(intent);
+                	Log.i("neha", "up");
+                }
+                
+                // Swipe down
+                else if (event2.getY() - event1.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+            		//Intent intent = new Intent(MainActivity.this, LanguageActivity.class);
+            		//startActivity(intent);
+                	Log.i("neha", "down");
+                }
+                // Swipe left
+                else if (event1.getX() - event2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+            		//Intent intent = new Intent(MainActivity.this, LanguageActivity.class);
+            		//startActivity(intent);
+                	Log.i("neha", "left");
+                }
+                // Swipe right
+                else {
+                	//Intent intent = new Intent(MainActivity.this, LanguageActivity.class);
+            		//startActivity(intent);
+                	Log.i("neha", "right");
+                }
+	            
+	            return true;
+	        }
+	    }
 	}
