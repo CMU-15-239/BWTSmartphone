6185b1294938790c279f5bd8afc1402fc0bd538b
Salem <salemhilal@gmail.com>
2013-04-03 20:51:21 -0400

    Added the frontend as static files. Now served up by '/'.

--- server.js
+++ server.js
@@ -8,12 +8,18 @@ app.configure(function(){
   app.use(express.bodyParser());
 });
 
+app.use(express.static(__dirname + '/public'));
+
+
 // words CRUD
 app.get('/words', words.findAll);
 app.get('/words/:id', words.findById);
 app.post('/words', words.addWord);
 app.put('/words/:id', words.updateWord);
 app.delete('/words/:id', words.deleteWord);
+
+app.get('/',express.static(__dirname + '/frontend'));
+
 
 app.listen(3000);
 console.log('Listening on port 3000...');
