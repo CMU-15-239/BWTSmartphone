01c540fad9c6d8734786a34c7b920541d95db4f0
Jeff <jmich@andrew.cmu.edu>
2013-04-08 23:07:00 -0400

    added put/delete

--- authRoutes.js
+++ authRoutes.js
@@ -2,46 +2,6 @@
 var User = require('./User');
 var Word = require('./Words')
 
-// var mongo = require('mongodb');
-// var Server = mongo.Server;
-// var Db = mongo.Db;
-// var BSON = mongo.BSONPure;
-
-
-// var server = new Server('localhost', 27017, {auto_reconnect : true});
-// db = new Db('wordsdb', server, {safe : true});
-
-// db.open(function(err,db) {
-//   if(!err) {
-//     console.log("Connected to 'wordsdb' database");
-//     db.collection('words', {safe:true}, function(err, collection) {
-//       if(err) {
-//         console.log("The 'words' collection doesn't exist. Creating it with sample data...");
-//         populateDB();
-//       }
-//     });
-//   }
-// });
-
-// var populateDB = function(){
-//   var words = [
-//     {
-//       word: "hello",
-//       def: "A simple greeting",
-//       pos: "exclamation",
-//       assns: []
-//     },
-//     {
-//       word: "world",
-//       def: "The earth, together with all of its countries, peoples, and natural features.",
-//       pos: "noun",
-//       assns: []
-//     }];
-
-//     db.collection('words', function(err, collection){
-//       collection.insert(words, {safe:true}, function(err, result) {});
-//     });
-// };
  
 // ******************************
 
@@ -106,38 +66,42 @@ module.exports = function (app) {
     });
 
 
-  app.put('/words/:id', function(req, res) {
-    var id = req.params.word;
-    var word = req.body;
-    console.log('Updating word: ' + id);
+  app.put('/words/:word', function(req, res) {
+    var word = req.params.word;
+    console.log('Updating word: ' + word);
     console.log(JSON.stringify(word));
-    db.collection('words', function(err, collection) {
-      collection.update({'_id':new BSON.ObjectID(id)}, wine, {safe:true}, function(err, result){
-        if (err) {
-          console.log('Error updating word: ' + err);
-        }
-        else {
-          console.log('' + result + 'document(s) updated');
-          res.send(word);
-        }
-      });
-    });
-  });
+    if ((req.user)){
+      Word.find({'word': word}, null, function(err, success){
+          if (err) {
+              console.log(err)
+              //throw err;
+          }
+          else {
+              Word.remove(success);
+              Word.create(req.body);
+              console.log('successful');
+              res.send(req.body);
+          }})
+    }
 
-  app.delete('/words/:id' , function(req,res) {
-    var id = req.params.id;
-    console.log('Deleting word: ' + id);
-    db.collection('words', function(err,collection) {
-      collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-        if (err) {
-          res.send({'error' : 'An error has occurred - ' + err});
-        }
-        else {
-          console.log('' + result + ' document(s) deleted');
-          res.send(req.body);
-        }
-      });
-    });
-  });
 
-};
+});
+
+  app.delete('/words/:word' , function(req,res) {
+    var word = req.params.word;
+    console.log('Updating word: ' + word);
+    console.log(JSON.stringify(word));
+    if ((req.user)){
+      Word.find({'word': word}, null, function(err, success){
+          if (err) {
+              console.log(err)
+              //throw err;
+          }
+          else {
+              Word.remove(success);
+              console.log('successful');
+              res.send(req.body);
+          }})
+    }
+  });
+}
