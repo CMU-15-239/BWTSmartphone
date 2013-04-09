d8aeaf05bcc9664c3b612e6c3d8d69cc7cadb3e9
Salem <salemhilal@gmail.com>
2013-04-09 18:29:57 -0400

    Code cleanup.

--- public/js/Unregistered.js
+++ public/js/Unregistered.js
@@ -1,73 +1,75 @@
 var App = function(){
-    this.registerEvents();
-}
+  this.registerEvents();
+};
 
 App.prototype.registerEvents = function(){
-    // this.registerRegister();
-    this.registerLogin();
-}
+  this.registerLogin();
+};
 
+// Wrapper for our POST reqyest. 
 App.prototype.ajaxFormJSON = function(json, url, onSuccess, onError){
-    var data = new FormData();
-    for (var key in json){
-        data.append(key, json[key]);
-    }
+  var data = new FormData();
+  for (var key in json){
+    data.append(key, json[key]);
+  }
 
-        $.ajax({
-            url: url,
-            data: data,
-            cache: false,
-            contentType: false,
-            processData: false,
-            type: 'POST',
-            success: onSuccess,
-            error: onError});
-}
+    $.ajax({
+      url: url,
+      data: data,
+      cache: false,
+      contentType: false,
+      processData: false,
+      type: 'POST',
+      success: onSuccess,
+      error: onError
+    });
+};
 
+// Gets the username and password and attempts to authenticate the session. 
 App.prototype.registerLogin = function(){
-    console.log($('#login'));
-    $('#login').click(function(){
-        var username = $('#loginUsername').val();
-        var password = $('#loginPassword').val();
+  $('#login').click(function(){
+    var username = $('#loginUsername').val();
+    var password = $('#loginPassword').val();
 
-        var data = new FormData();
-        data.append('username', username);
-        data.append('password', password);
+    var data = new FormData();
+    data.append('username', username);
+    data.append('password', password);
 
-        this.ajaxFormJSON(
-                {
-                    username: username,
-                    password: password
-                },
-                '/login',
-                function success(data){
-                    if (data === 'success'){
-                        window.location = '/';
-                    }
-                    else {
-                        console.log(JSON.stringify(data));
-                    }
-                },
-                function error(xhr, status, err){
-                    if(err){
-                        $("input").addClass("error-custom").removeClass("disabled-custom");
-                    }
-                });
-    }.bind(this));
+    this.ajaxFormJSON(
+    {
+      username: username,
+      password: password
+    },
+    '/login',
+    function success(data){
+      if (data === 'success'){
+        window.location = '/';
+      }
+      else {
+        console.log(JSON.stringify(data));
+      }
+    },
+    function error(xhr, status, err){
+      if(err){
+        $("input").addClass("error-custom").removeClass("disabled-custom");
+      }
+    });
+  }.bind(this));
 };
 
 
 $(document).ready(function(){
-new App();
+  // Initialize the login app.
+  new App();
 
-
-$("#loginPassword").keyup(function(event){
-  if(event.keyCode === 13){
+  // Bind enter to the submit button. 
+  $("#loginPassword").keyup(function(event){
+    if(event.keyCode === 13){
     $("#login").click();
     $("input").addClass("disabled-custom");
-  }
-});
+    }
+  });
 
-$("#loginUsername").focus();
+  $("#loginUsername").focus();
 
 });
